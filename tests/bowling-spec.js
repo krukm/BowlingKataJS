c274fbbcc3723eb65454bdd3563964ed73615b7f
Matthew Kruk <matthew.kruk@gmail.com>
2018-04-16 08:52:53 -0400

    test for 20 1pin rolls

--- tests/bowling-spec.js
+++ tests/bowling-spec.js
@@ -7,17 +7,23 @@ describe('Bowling scorer', function () {
       this.game = new BowlingGame();
    });
 
-   // This test should be passing
    it('should return a score of 0 if no pins have been knocked down', function () {
       var score = this.game.score();
       assert.equal(score, 0);
    });
 
-   // This test should be failing at this point
    it('should return a score of 1 if 1 pin has been knocked down', function () {
       this.game.roll(1);
       var score = this.game.score();
       assert.equal(score, 1);
+   });
+
+   it('should return a score of 20 for 20 1 pin rolls', function() {
+     for(i = 0; i < 20; i++) {
+       this.game.roll(1);
+     }
+     var score = this.game.score();
+     assert.equal(score, 20);
    });
 
 });
